f7f0f4a424935732c18ff69ce96fdf1e0ec6dfca
Russell Dow <russdow1@hotmail.com>
2024-12-11 11:57:15 -0800

    add a post route for the same

--- app/api/sp/route.ts
+++ app/api/sp/route.ts
@@ -10,3 +10,14 @@ export async function GET(req: NextRequest) {
     },
   });
 }
+
+export async function POST(req: NextRequest) {
+  const token = req.nextUrl.searchParams.get("validationToken");
+
+  return new NextResponse(token, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+}
